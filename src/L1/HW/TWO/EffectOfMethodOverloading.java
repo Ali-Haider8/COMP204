bf4dd6e6ec10ef46b68aec9b88b73f987eb32772
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-11 23:58:31 +0300

    make new class

---
+++ src/L1/HW/TWO/EffectOfMethodOverloading.java
@@ -0,0 +1,13 @@
+package L1.HW.TWO;
+
+/*
+Source: .pdf (page 1 of 1)
+Created by ali_h on 7/11/2026 at 11:58 PM
+*/
+
+public class EffectOfMethodOverloading {
+
+    public static void main(String[] args) {
+
+    }
+}
