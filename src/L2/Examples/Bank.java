6fe21ce666c035d8c31bc0c27a7521ca4e006ef1
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-07-15 18:49:28 +0300

    write a code example from lect

---
+++ src/L2/Examples/Bank.java
@@ -0,0 +1,16 @@
+package L2.Examples;
+
+/*
+Source: .pdf (page 1 of 1)
+Created by ali_h on 7/15/2026 at 18:45
+*/
+
+public class Bank {
+
+    public static void main(String[] args) {
+        BankAccount customer = new BankAccount();
+//        customer.balance = 100000;
+        customer.setBalance(100000);
+        System.out.println("Balance Information: " + customer.getBalance());
+    }
+}
